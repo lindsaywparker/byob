84656a8a18d4a2f0fc5f3c672ff7046479123a2c
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-08-27 13:41:15 -0600

    WIP: hard-code .test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -4,9 +4,9 @@ const chaiHttp = require('chai-http');
 const should = chai.should();
 const server = require('../server');
 
-const environment = process.env.NODE_ENV || 'test';
-const configuration = require('../knexfile')[environment];
-const configuration = require('../knexfile')[environment];
+// const environment = process.env.NODE_ENV || 'test';
+// const configuration = require('../knexfile')[environment];
+const configuration = require('../knexfile').test;
 const knex = require('knex')(configuration);
 
 chai.use(chaiHttp);
